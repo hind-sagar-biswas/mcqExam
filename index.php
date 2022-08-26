2c5afe21f858e7f49aa92209272d238f624626e6
Hind Sagar Biswas <hindsbhk@gmail.com>
2022-08-26 13:45:31 +0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+ declare(strict_types = 1);
+ require "includes/autoloader.inc.php";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Sample Title</title>
+  <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+</body>
+</html>
