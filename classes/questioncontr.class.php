761e99cb0604f55554046c6f753d7f5a37d13158
Hind Sagar Biswas <hindsbhk@gmail.com>
2022-10-17 10:56:18 +0600

    fixing question class 1

--- classes/questioncontr.class.php
+++ classes/questioncontr.class.php
@@ -3,8 +3,59 @@ require '../classes/dbh.class.php';
 
 class QuestionContr extends Dbh
 {
+  private $cls;
+  private $subject;
+  private $chapter;
+  private $topic;
+  private $subTopic;
+  private $topicId;
+  private $subTopicId;
+  private $image;
+  private $question;
+  private $answer;
+  private $op1;
+  private $op1Img;
+  private $op2;
+  private $op2Img;
+  private $op3;
+  private $op3Img;
+  private $op4;
+  private $op4Img;
+  private $opNs;
+  private $optionId;
+  private $refImg;
+  private $ref ;
+  private $refLink; 
 
-  public function getTopicId($topic, $type)
+  public function __construct($cls, $subject, $chapter, $topic, $subTopic, $image, $question, $answer, $op1, $op1Img, $op2, $op2Img, $op3, $op3Img, $op4, $op4Img, $opNs, $refImg, $ref , $refLink)
+  {
+    $this->cls = $cls;
+    $this->subject = $subject;
+    $this->chapter = $chapter;
+    $this->topic = $topic;
+    $this->subTopic = $subTopic;
+    $this->image = $image;
+    $this->question = $question;
+    $this->answer = $answer;
+    $this->op1 = $op1;
+    $this->op1Img = $op1Img;
+    $this->op2 = $op2;
+    $this->op2Img = $op2Img;
+    $this->op3 = $op3;
+    $this->op3Img = $op3Img;
+    $this->op4 = $op4;
+    $this->op4Img = $op4Img;
+    $this->opNs = $opNs;
+    $this->refImg = $refImg;
+    $this->ref = $ref;
+    $this->refLink = $refLink; 
+
+    $this->topicId = $this->getTopicId($this->topic, "main");
+    $this->subTopicId = (!empty($this->subTopic)) ? $this->getTopicId($this->subTopic, "sub", $this->topicId); : 'NULL'; ;
+
+  }
+
+  public function getTopicId($topic, $type, $parent = 'NULL')
   {
     $getquery = "SELECT id FROM topics WHERE topic_name='$topic' AND topic_type='$type'";
     $id = mysqli_fetch_assoc(mysqli_query($this->connect(), $getquery));
